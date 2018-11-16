c3e866bc16c20375c409caa8fabb8cdb429a1a04
Zainab-Abdulmanea <zezo.saz@gmail.com>
2018-11-16 23:59:27 +0300

    Define Database connection to 'news db'

--- logs_reporting.py
+++ logs_reporting.py
@@ -2,29 +2,38 @@
 
 import psycopg2
 
-db = psycopg2.connect("dbname=news")
+
+def get_results(query):
+    """Return all results from 'news database"""
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute("select content, time from posts order by time desc")
+    posts = c.fetchall()
+    db.close()
+    return posts
+
 
 # 1. What are the most popular three articles of all time?
-'''
+"""
 SELECT articles.title AS "Popular Articles", COUNT(*) AS Views
 FROM log JOIN articles
 ON articles.slug = SUBSTRING(log.path,10)
 GROUP BY articles.title
 ORDER BY Views DESC LIMIT 3;
-'''
+"""
 
 # 2. Who are the most popular article authors of all time?
-'''
+"""
 SELECT authors.name, COUNT(*) AS Views
 FROM log, articles, authors
 WHERE SUBSTRING(log.path,10) = articles.slug
 AND articles.author = authors.id
 GROUP BY authors.name
 ORDER BY views DESC;
-'''
+"""
 
 # 3. On which days did more than 1% of requests lead to errors?
-'''
+"""
 WITH logs_status AS(
 	SELECT time::date AS date, status, COUNT(*) AS status_num
 	FROM log
@@ -34,4 +43,4 @@ SELECT a.date, Round((b.status_num*1.0/(a.status_num+b.status_num))*100, 2) AS e
 FROM logs_status a JOIN logs_status b
 ON a.date = b.date AND a.status < b.status
 WHERE (b.status_num*1.0/(a.status_num+b.status_num)*100) > 1;
-'''
+"""
