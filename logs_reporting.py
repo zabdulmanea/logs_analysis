80a15990da9187ddf32905261240096502843218
Zainab-Abdulmanea <zezo.saz@gmail.com>
2018-11-17 16:38:50 +0300

    Write view results into file

--- logs_reporting.py
+++ logs_reporting.py
@@ -14,14 +14,13 @@ queries = [
       FROM log JOIN articles
       ON articles.slug = SUBSTRING(log.path,10)
       GROUP BY articles.title
-      ORDER BY Views DESC LIMIT 3;
-    """, """ SELECT authors.name, COUNT(*) AS Views
+      ORDER BY Views DESC LIMIT 3 """,
+    """ SELECT authors.name, COUNT(*) AS Views
         FROM log, articles, authors
         WHERE SUBSTRING(log.path,10) = articles.slug
         AND articles.author = authors.id
         GROUP BY authors.name
-        ORDER BY views DESC;
-    """, """ WITH logs_status AS(
+        ORDER BY views DESC; """, """ WITH logs_status AS(
             SELECT time::date AS date, status, COUNT(*) AS status_num
             FROM log
             GROUP BY time::date, status ) 
@@ -45,21 +44,27 @@ def get_result(query):
 
 
 def view_result():
-    print('-- Log Analysist Reporting Tool Resluts --')
+    logs_file = open("logs_results.txt", "w+")
+    logs_file.write('-- Log Analysis Reporting Tool Resluts --\r\n')
     i = 0
     while i < len(questions):
-        print('\n' + questions[i] + ':')
+        logs_file.write("\r\n* %s:\r\n" % questions[i])
         query_results = get_result(queries[i])
         j = 0
         while j < len(query_results):
-            if i != 2:
-                print('"' + query_results[j][0] + '" — ' +
-                      str(query_results[j][1]) + ' views')
+            if i == 0:
+                logs_file.write('"' + query_results[j][0] + '" — ' +
+                                str(query_results[j][1]) + ' views')
+            elif i == 1:
+                logs_file.write(query_results[j][0] + ' — ' +
+                                str(query_results[j][1]) + ' views')
             else:
-                print(query_results[j][0] + ' — ' + str(query_results[j][1]) +
-                      '% errors')
+                logs_file.write(query_results[j][0] + ' — ' +
+                                str(query_results[j][1]) + '% errors')
+            logs_file.write('\r\n')
             j += 1
         i += 1
+    logs_file.close()
 
 
 if __name__ == '__main__':
