5431e83c77f028a255f0142e67373c06f6e13b61
Zainab-Abdulmanea <zezo.saz@gmail.com>
2018-11-17 17:06:03 +0300

    Follow pep8 code quality checker

--- logs_reporting.py
+++ logs_reporting.py
@@ -3,12 +3,14 @@
 
 import psycopg2
 
+# The questions list should be answered by db queries
 questions = [
     "The most popular three articles of all time",
     "The most popular article authors of all time",
     "Days with more than 1% of requests lead to errors",
 ]
 
+# The queries to answer the questions list
 queries = [
     """ SELECT articles.title AS "Popular Articles", COUNT(*) AS Views
       FROM log JOIN articles
@@ -20,11 +22,12 @@ queries = [
         WHERE SUBSTRING(log.path,10) = articles.slug
         AND articles.author = authors.id
         GROUP BY authors.name
-        ORDER BY views DESC; """, """ WITH logs_status AS(
+        ORDER BY views DESC; """,
+    """ WITH logs_status AS(
             SELECT time::date AS date, status, COUNT(*) AS status_num
             FROM log
-            GROUP BY time::date, status ) 
-        SELECT to_char(a.date, 'FMMonth DD, YYYY'), 
+            GROUP BY time::date, status )
+        SELECT to_char(a.date, 'FMMonth DD, YYYY'),
         Round((b.status_num*1.0/(a.status_num+b.status_num))*100, 2) AS error
         FROM logs_status a JOIN logs_status b
         ON a.date = b.date AND a.status < b.status
@@ -34,7 +37,7 @@ queries = [
 
 
 def get_result(query):
-    """Return all results from 'news database"""
+    """Return all queries results from 'news database"""
     db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
     cursor.execute(query)
@@ -44,25 +47,30 @@ def get_result(query):
 
 
 def view_result():
+    """Get queries results and write results into new text file"""
+
     logs_file = open("logs_results.txt", "w+")
     logs_file.write('-- Log Analysis Reporting Tool Resluts --\r\n')
+
     i = 0
     while i < len(questions):
         logs_file.write("\r\n* %s:\r\n" % questions[i])
         query_results = get_result(queries[i])
+
         j = 0
         while j < len(query_results):
             if i == 0:
-                logs_file.write('"' + query_results[j][0] + '" — ' +
+                logs_file.write('"' + query_results[j][0] + '" - ' +
                                 str(query_results[j][1]) + ' views')
             elif i == 1:
-                logs_file.write(query_results[j][0] + ' — ' +
+                logs_file.write(query_results[j][0] + ' - ' +
                                 str(query_results[j][1]) + ' views')
             else:
-                logs_file.write(query_results[j][0] + ' — ' +
+                logs_file.write(query_results[j][0] + ' - ' +
                                 str(query_results[j][1]) + '% errors')
             logs_file.write('\r\n')
             j += 1
+
         i += 1
     logs_file.close()
 
